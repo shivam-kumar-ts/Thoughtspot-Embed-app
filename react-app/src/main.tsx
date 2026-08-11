e6b88383ebc9b5a547dcbc2ec2c64e2d7bcdaa80
Shivam Kumar <shivam.kumar@thoughtspot.com>
2026-08-11 09:14:21 +0530

    added the auth flow

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useContext, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import App from "./App.tsx";
@@ -7,15 +7,40 @@ import Header from "./components/header/index.tsx";
 import NotificationStack from "./components/notification/index.tsx";
 import { AppContextProvider } from "./contexts/AppContext.tsx";
 import { NotificationProvider } from "./contexts/NotificationContext.tsx";
+import NotificationContext from "./contexts/NotificationContext.tsx";
+import { authenticate } from "./utils/auth.ts";
+import { NOTIFICATION_TYPE } from "./types/index.ts";
+import { NOTIFICATION_MESSAGES } from "./utils/constants.ts";
+
+function AppInit() {
+  const { notify } = useContext(NotificationContext);
+
+  useEffect(() => {
+    authenticate((err) => {
+      notify(
+        NOTIFICATION_TYPE.ERROR,
+        NOTIFICATION_MESSAGES.auth.failed,
+        err.message || NOTIFICATION_MESSAGES.auth.message,
+      );
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <NotificationStack />
+      <Header />
+      <App />
+      <Footer />
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NotificationProvider>
       <AppContextProvider>
-        <NotificationStack />
-        <Header />
-        <App />
-        <Footer />
+        <AppInit />
       </AppContextProvider>
     </NotificationProvider>
   </StrictMode>,
